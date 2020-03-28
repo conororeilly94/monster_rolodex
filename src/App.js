c46079de4a2287b9ff62ec91e9caa149bbc6319f
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-28 14:01:31 +0000

    card list component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // Dsiplay list of monsters
+      // Array of objects
+      monsters: []
+    };
+  }
+
+  // Renders onto the DOM for 1st time and calls code written below
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Conor
-            </p>
-          <button>Change Text</button>
-        </header>
+        <CardList name="Conor">
+          {
+            this.state.monsters.map(monster => (
+              // map returns function we call and iterates over every element in the function
+              // map creates new arrays on our existing arrays 
+              <h1 key={monster.id}> {monster.name} </h1>
+            ))}
+        </CardList>
       </div>
     );
   }
